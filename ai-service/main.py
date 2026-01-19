e03cbb4796ee4baa41a36059b9757b3306dac46b
HarikrishnaEP <163709535+HarikrishnaEP@users.noreply.github.com>
2026-01-19 14:53:20 +0530

    agent return data in strectured format

--- ai-service/main.py
+++ ai-service/main.py
@@ -1,3 +1,7 @@
+from typing import List
+
+from pydantic import BaseModel, Field
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,30 +12,40 @@ from langchain_core.messages import HumanMessage
 #from langchain_openai import ChatOpenAI
 from langchain_groq import ChatGroq
 from tavily import TavilyClient
+from langchain_tavily import TavilySearch
 
-tavily = TavilyClient()
 
-@tool
-def search(query: str) -> str:
-    """
-    Tool that searches over internet
-    Args:
-        query: The query to search for
-    Returns:
-        The search result
-    """
-    print(f"Searching for {query}")
-    return tavily.search(query=query, max_results=3)
+# @tool
+# def search(query: str) -> str:
+#     """
+#     Tool that searches over internet
+#     Args:
+#         query: The query to search for
+#     Returns:
+#         The search result
+#     """
+#     print(f"Searching for {query}")
+#     return tavily.search(query=query, max_results=3)
 
+class Source(BaseModel):
+    """Schema for source used by agent"""
+
+    url:str = Field(..., description="The URL of the source")
+
+class AgentResponse(BaseModel):
+    """Schema for agent response with answer and sources"""
+
+    answer: str = Field(..., description="The agent answer to the query")
+    sources: List[Source] = Field(default_factory=list, description="The list of sources used by the agent")
 
 llm = ChatGroq(temperature=0,model="llama-3.1-8b-instant")
-tools = [search]
-agent = create_agent(model=llm,tools=tools)
+tools = [TavilySearch()]
+agent = create_agent(model=llm,tools=tools, response_format = AgentResponse) # Java equivalent: Agent agent = AgentFactory.createAgent(llm, tools);
 
 
 def main():
     print("Hello from langchain-course!")
-    result = agent.invoke({"messages":HumanMessage(content="search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details")})
+    result = agent.invoke({"messages":HumanMessage(content="what is the capital of france?")})
     print(result)
 
 if __name__ == "__main__":
