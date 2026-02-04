4246265d5a621491bc5c331ebd5ed195f02c10e1
HarikrishnaEP <163709535+HarikrishnaEP@users.noreply.github.com>
2026-02-04 15:13:07 +0530

    added ingestion and retrival pipeline

--- ai-service/main.py
+++ ai-service/main.py
@@ -1,52 +1,28 @@
-from typing import List
-
-from pydantic import BaseModel, Field
-
 from dotenv import load_dotenv
 
 load_dotenv()
-from langchain.agents import create_agent
-from langchain.tools import tool
-from langchain_core.messages import HumanMessage
 
-#from langchain_openai import ChatOpenAI
+# from langchainhub import pull
+# from langchainhub import hub
+# from langchain.agents.react.agent import create_agent
+# from langchain.agents.react.prompt import REACT_PROMPT
+from langchain.agents import create_agent
 from langchain_groq import ChatGroq
-from tavily import TavilyClient
 from langchain_tavily import TavilySearch
 
-
-# @tool
-# def search(query: str) -> str:
-#     """
-#     Tool that searches over internet
-#     Args:
-#         query: The query to search for
-#     Returns:
-#         The search result
-#     """
-#     print(f"Searching for {query}")
-#     return tavily.search(query=query, max_results=3)
-
-class Source(BaseModel):
-    """Schema for source used by agent"""
-
-    url:str = Field(..., description="The URL of the source")
-
-class AgentResponse(BaseModel):
-    """Schema for agent response with answer and sources"""
-
-    answer: str = Field(..., description="The agent answer to the query")
-    sources: List[Source] = Field(default_factory=list, description="The list of sources used by the agent")
-
-llm = ChatGroq(temperature=0,model="llama-3.1-8b-instant")
 tools = [TavilySearch()]
-agent = create_agent(model=llm,tools=tools, response_format = AgentResponse) # Java equivalent: Agent agent = AgentFactory.createAgent(llm, tools);
+llm = ChatGroq(temperature=0, model="llama-3.1-8b-instant")
+agent = create_agent(model=llm, tools=tools)
 
 
+# agent = create_agent(model = llm, tools = tools)
 def main():
-    print("Hello from langchain-course!")
-    result = agent.invoke({"messages":HumanMessage(content="what is the capital of france?")})
+    print("Hello from LangChain!")
+    result = agent.invoke(
+        {"messages": [("user", "Who is the president of the United States?")]}
+    )
     print(result)
+
 
 if __name__ == "__main__":
     main()
